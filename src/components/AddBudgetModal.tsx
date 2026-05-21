866cd743faa3510312efc0631a01ec812cc97df2
keti <qeti.tsiklauri320@ens.tsu.edu.ge>
2026-05-21 19:11:42 +0400

    style:fix addnewbudgets modals styles

--- src/components/AddBudgetModal.tsx
+++ src/components/AddBudgetModal.tsx
@@ -31,14 +31,17 @@ export default function AddBudgetModal({
 
   return (
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
-      <div className="w-[560px] h-[490px] bg-white rounded-[12px] p-8 flex flex-col gap-5">
+      <div className="w-[300px] md:w-[560px] h-[490px] bg-white rounded-[12px] p-8 flex flex-col gap-5">
         {/* TITLE */}
         <div className="flex justify-between items-center">
-          <h1 className="text-[32px] font-bold text-[#201F24]">
+          <h1 className="text-[20px] md:text-[32px] font-bold text-[#201F24]">
             Add New Budget
           </h1>
 
-          <button onClick={onClose} className="w-8 h-8 cursor-pointer">
+          <button
+            onClick={onClose}
+            className="w-6 h-6 md:w-8 md:h-8 cursor-pointer"
+          >
             <Image
               src="/images/icon-close.svg"
               alt="close"
@@ -75,9 +78,16 @@ export default function AddBudgetModal({
             </label>
             <input
               value={maximum}
-              onChange={(e) => setMaximum(e.target.value)}
+              onChange={(e) => {
+                const value = e.target.value;
+
+                if (Number(value) < 0) return;
+
+                setMaximum(value);
+              }}
               placeholder="e.g. 2000"
               type="number"
+              min={0}
               className="w-full mt-1 h-[45px] border border-[#98908B] rounded-lg px-4 outline-none"
             />
           </div>
@@ -100,7 +110,7 @@ export default function AddBudgetModal({
         {/* BUTTON */}
         <button
           onClick={handleSubmit}
-          className="w-full h-[53px] bg-[#201F24] text-white rounded-lg font-bold"
+          className="w-full h-[53px] bg-[#201F24] text-white rounded-lg font-bold cursor-pointer"
         >
           Add Budget
         </button>
