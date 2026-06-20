851ea5fa97a26fcd73478e3f441d0296e6566a7d
keti <qeti.tsiklauri320@ens.tsu.edu.ge>
2026-06-20 16:10:54 +0400

    save pots in localstorage

--- src/app/pots/page.tsx
+++ src/app/pots/page.tsx
@@ -4,12 +4,37 @@ import AddNewPotModal from "@/src/components/AddNewPotModal";
 import Header from "@/src/components/Header";
 import PotsList from "@/src/components/PotsList";
 import { Pot } from "@/src/types/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { pots as initialPots } from "@/src/data/data-pots";
 
 export default function Pots() {
-  const [pots, setPots] = useState<Pot[]>(initialPots);
   const [showAddPotModal, setShowAddPotModal] = useState(false);
+  const [pots, setPots] = useState<Pot[]>(initialPots);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const STORAGE_KEY = "pots";
+
+  // LOAD
+  useEffect(() => {
+    const saved = localStorage.getItem(STORAGE_KEY);
+
+    if (saved) {
+      setPots(JSON.parse(saved));
+    }
+
+    setIsLoaded(true);
+  }, []);
+
+  // SAVE (ONLY ONCE)
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(pots));
+  }, [pots, isLoaded]);
+
+  if (!isLoaded) {
+    return <div className="p-6">Loading...</div>;
+  }
 
   return (
     <div className="bg-[#F8F4F0] flex flex-col xl:gap-[32px] gap-[20px]">
